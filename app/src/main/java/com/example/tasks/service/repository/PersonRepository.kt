6f33f2af2ee5bcba0824f96d8d98640010bacd26
diegodamascenodev <diegodamasceno.dev@gmail.com>
2020-09-17 11:55:32 -0300

    - Passando os dados respondidos para a LoginViewModel.kt para que na proxima aula possamos passar para o Observe e terminar a implementação do Login nas proximas duas aulas. - Fim da aula 179

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,6 +1,7 @@
 package com.example.tasks.service.repository
 
 import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
 import retrofit2.Call
@@ -11,19 +12,19 @@ class PersonRepository {
 
     private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
-    fun login(email: String, password: String) {
+    fun login(email: String, password: String, listener: APIListener) {
 
         val call: Call<HeaderModel> = mRemote.login(email, password)
         //Assíncrona
         call.enqueue(object : Callback<HeaderModel>{
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
-                val header = response.body()
+                response.body()?.let { listener.onSuccess(it) }                                     //Esse codigo verifica se o "response.body()" é nulo. Se não for o response.body() será passado como argumento no "listener.onSuccess()". Sim, o "it" está fazendo o papael do "response.body()". Era mais intuitivo se fizermos como foi feito na linha comentada abaixo. Mas quando colocamos desse jeito a própria IDE fala que esse valor pode ser nulo e te da a opção de transformar o código comentado abaixo no código usado nessa linha.
+                //listener.onSuccess(response.body())
             }
 
             override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
-                val s = ""
+                listener.onFailure((t.message.toString()))
             }
-
         })
         //remote.login(email, password)
     }
