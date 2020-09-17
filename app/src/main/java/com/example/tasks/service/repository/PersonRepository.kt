a46a2ab115fd06865d5c59cc2d9bec319644df66
diegodamascenodev <diegodamasceno.dev@gmail.com>
2020-09-17 13:45:09 -0300

    - Ainda implementando login - Fim da aula 180

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,9 +1,11 @@
 package com.example.tasks.service.repository
 
 import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,7 +20,13 @@ class PersonRepository {
         //Assíncrona
         call.enqueue(object : Callback<HeaderModel>{
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
-                response.body()?.let { listener.onSuccess(it) }                                     //Esse codigo verifica se o "response.body()" é nulo. Se não for o response.body() será passado como argumento no "listener.onSuccess()". Sim, o "it" está fazendo o papael do "response.body()". Era mais intuitivo se fizermos como foi feito na linha comentada abaixo. Mas quando colocamos desse jeito a própria IDE fala que esse valor pode ser nulo e te da a opção de transformar o código comentado abaixo no código usado nessa linha.
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation = Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(response.errorBody()!!.string())
+                } else{
+                    response.body()?.let { listener.onSuccess(it) }                                 //Esse codigo verifica se o "response.body()" é nulo. Se não for o response.body() será passado como argumento no "listener.onSuccess()". Sim, o "it" está fazendo o papael do "response.body()". Era mais intuitivo se fizermos como foi feito na linha comentada abaixo. Mas quando colocamos desse jeito a própria IDE fala que esse valor pode ser nulo e te da a opção de transformar o código comentado abaixo no código usado nessa linha.
+                }
+
                 //listener.onSuccess(response.body())
             }
 
--- app/src/main/java/com/example/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/example/tasks/view/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
 import com.example.tasks.viewmodel.LoginViewModel
@@ -54,7 +56,13 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
      * Observa ViewModel
      */
     private fun observe() {
-
+        mViewModel.login.observe(this, Observer{
+            if (it) {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                Toast.makeText(this, "Erro ao login", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     /**
